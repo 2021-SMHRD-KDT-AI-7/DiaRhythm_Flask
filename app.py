ae8a937a0a063f894abe5200e561d66bd4ec57f1
박진주 <54519997+pjj7367@users.noreply.github.com>
2022-01-29 14:45:53 +0900

    app.py 코드 복구 및 테스트 완료 - 실행 환경 구축 완료 - 일부 라이브러리 및 .pt 파일은 미작업

---
+++ app.py
@@ -0,0 +1,46 @@
+# app.py
+
+from flask import Flask
+from sqlalchemy.engine import create_engine
+import pandas as pd
+
+app = Flask(__name__)
+
+# DB Connection Setting
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+USERNAME = 'onion' #enter your username
+PASSWORD = 'smhrd123' #enter your password
+HOST = 'project-db-stu.ddns.net' #enter the oracle db host url
+PORT = 1524 # enter the oracle port number
+SERVICE = 'xe' # enter the oracle db service name
+ENGINE_PATH_WIN_AUTH = DIALECT + '+' + SQL_DRIVER + '://' + USERNAME + ':' + PASSWORD +'@' + HOST + ':' + str(PORT) + '/?service_name=' + SERVICE
+
+engine = create_engine(ENGINE_PATH_WIN_AUTH)
+
+#test query
+test_df = pd.read_sql_query('SELECT * FROM tbl_member', engine)
+#
+
+# temp code --> 모델 불러오기
+# 1. 불러온 모델로 결과값을 변수에 저장.
+#model = torch.load('model_kobert.pt')
+
+# result = model.eval()
+#
+@app.route('/')
+def hello_world():
+    # 2. 여기가 홈페이지에서 보이는 부분.
+    # 이클립스 서블릿이 안만들어져 있으니깐 여기서 표시하는건 단순한 String만 가능!
+    # 1번에서 저장했던 변수에 .to_string() 을 써서 String 출력
+
+    # return result.to_string()
+    return test_df.to_string()
+
+#host_addr = 'project-db-stu.ddns.net'
+host_addr = 'localhost'
+port_num = 1524
+
+#파이썬 명령어로 실행할 수 있음
+if __name__ == '__main__':
+    app.run(host=host_addr, port=port_num,  debug=True)
